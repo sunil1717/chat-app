f32b3dbdaed3b7b61d0e367fcf91b62c0c265198
sunil sahoo <sunilsahoo745757@gmail.com>
2025-04-16 13:39:13 +0530

    Update utills.js

--- backend/src/lib/utills.js
+++ backend/src/lib/utills.js
@@ -9,7 +9,7 @@ export const generatetoken =(userId,res)=>{
    res.cookie("jwt",token,{
      maxAge:7*24*60*1000,
      httpOnly:true,
-     sameSite:"strict",
+     sameSite:"none",
      secure:process.env.NODE_ENV !="development"
    })
    return token;
