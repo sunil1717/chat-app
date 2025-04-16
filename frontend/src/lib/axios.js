4781756b28155467a6f6ce989425e418a979bdf4
sunil sahoo <sunilsahoo745757@gmail.com>
2025-04-16 11:05:27 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chat-app-b.vercel.app/api",
     withCredentials: true,
 
 });
